b14daf5fa9db3f136eb8b48a6e73e2989918ed21
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-11-03 22:45:32 -0400

    Refactor application to accommodate hosting more of a gallery approach to the demos - Move initial 2D demo/map to a "sub-page" - Create a proper home / index page. - Create a 3D basic demo for the/a SceneView

---
+++ public_html/js/Models.js
@@ -0,0 +1,26 @@
+/***********************
+ *	Project		: ArcGIS-2d
+ *	Author		: Todd Mullen
+ *	Document	: Models.js
+ *	Created		: Nov 3, 2017, 9:30:46 PM
+ *	Description	:
+ *      Controls
+ ***********************/
+
+function Page( relPath, name, title, cat ){
+	return {
+		path: relPath
+		,name: name
+		,title: title
+		,category: cat
+	};
+}
+
+Page.prototype.CAT_2D = "2d";
+Page.prototype.CAT_3D = "3d";
+
+
+function PageBtn( page ){
+
+}
+
---
+++ public_html/js/menu.js
@@ -0,0 +1,19 @@
+/***********************
+ *	Project		: ArcGIS-2d
+ *	Author		: Todd Mullen
+ *	Document	: menu.js
+ *	Created		: Nov 3, 2017, 9:29:51 PM
+ *	Description	:
+ *      Controls
+ ***********************/
+
+/* global Page, $ */
+
+pageAry = [
+	Page("basic-2d.html", "2d MapView", "Basic 2D MapView", Page.CAT_2D )
+	,Page( "basic-3d.html", "#d SceneView", "Intro to SceneView - Create a 3D map", Page.CAT_3D )
+];
+
+
+
+
