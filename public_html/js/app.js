4de58f783e0b9fb7c32179789171e542c33dd75b
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-11-03 21:06:13 -0400

    ArcGIS test demo 1 initial commit

---
+++ public_html/js/app.js
@@ -0,0 +1,14 @@
+/***********************
+ *	Project		: ArcGIS-2d
+ *	Author		: Todd Mullen
+ *	Document	: app.js
+ *	Created		: Nov 3, 2017, 8:57:02 PM
+ *	Description	:
+ *      Controls
+ ***********************/
+
+/* global  */
+
+
+
+
