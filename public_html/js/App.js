08dc5a365b8fbc8dc1277f8f51364fa150ebc5c6
Todd Mullen <ToddJ.Mullen@Gmail.com>
2017-11-07 20:22:38 -0500

    Add "streets-night-vector" base map type - Correct position top-left constant.

--- public_html/js/App.js
+++ public_html/js/App.js
@@ -12,24 +12,26 @@
 
 var app = {
 	MAP_TYPE	: {
-		STREETS			: "streets"
-		,TOPO			: "topo"
-		,SATELLITE		: "satellite"
-		,HYBRID			: "hybrid"
-		,GRAY			: "gray"
-		,GRAY_DARK		: "dark-gray"
-		,OCEANS			: "oceans"
-		,OSM			: "osm"
-		,NAT_GEO		: "national-geographic"
+		STREETS					: "streets"
+		,STREET_NIGHTS_VECTOR	: "streets-night-vector"
+		,TOPO					: "topo"
+		,SATELLITE				: "satellite"
+		,HYBRID					: "hybrid"
+		,GRAY					: "gray"
+		,GRAY_DARK				: "dark-gray"
+		,OCEANS					: "oceans"
+		,OSM					: "osm"
+		,NAT_GEO				: "national-geographic"
 	}
 	,MAP_COORDS: {
 		BOISE_IDAHO		: [-116.3031, 43.6088]
 		,INDIANA		: [-86.049, 38.485]
 		,SWEDEN_NORTH	: [15, 65]
+		,WASHINGTON_DC	: [-77.02, 38.89]
 	}
 	,POSITION	: {
 		TOP_RIGHT	: "top-right"
-		,TOP_RIGHT	: "top-left"
+		,TOP_LEFT	: "top-left"
 		,BOT_LEFT	: "bottom-left"
 		,BOT_RIGHT	: "bottom-right"
 	}
